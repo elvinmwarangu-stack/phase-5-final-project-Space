0e83095a1057aa30f76b9e47261d0ac05ce41d8e
Elvin <elvin.mwarangu@student.moringaschool.com>
2026-02-03 11:40:02 +0300

    more code

--- Backend/app/core/dependencies.py
+++ Backend/app/core/dependencies.py
@@ -6,3 +6,12 @@ from sqlalchemy.orm import Session
 from app.database.session import SessionLocal
 from app.database.models import User
 from app.config import settings
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
