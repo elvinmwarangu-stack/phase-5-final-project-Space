f5c9f96bfdd4edec9fd5a1115b528783439e5cc5
Elvin <elvin.mwarangu@student.moringaschool.com>
2026-02-03 09:46:22 +0300

    auth routes

--- Backend/app/auth/routes.py
+++ Backend/app/auth/routes.py
@@ -1,2 +1,15 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
+from app.database.session import SessionLocal
+from app.database.session import SessionLocal
+from app.auth.schemas import (
+    RegisterRequest,
+    LoginRequest,
+    TokenResponse
+)
+from app.auth.service import (
+    register_user,
+    authenticate_user
+)
+
+router = APIRouter()
