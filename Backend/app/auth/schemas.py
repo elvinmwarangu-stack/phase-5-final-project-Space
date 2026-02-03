fcfa7c5236450995de2bf00955ad1e93c0f0b9d3
Elvin <elvin.mwarangu@student.moringaschool.com>
2026-02-03 11:29:20 +0300

    more code

--- Backend/app/auth/schemas.py
+++ Backend/app/auth/schemas.py
@@ -3,4 +3,7 @@ from pydantic import BaseModel, EmailStr
 class RegisterRequest(BaseModel):
     name: str
     email: EmailStr
+
+class LoginRequest(BaseModel):
+    email: EmailStr
     password: str
