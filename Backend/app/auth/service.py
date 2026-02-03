d58f73087c9f5235db2a694335e792e0148b2086
Elvin <elvin.mwarangu@student.moringaschool.com>
2026-02-03 11:37:14 +0300

    final service code

--- Backend/app/auth/service.py
+++ Backend/app/auth/service.py
@@ -7,6 +7,8 @@ from app.core.security import (
     verify_password,
     create_access_token
 )
+
+
 def register_user(db: Session, name: str, email: str, password: str):
     existing_user = db.query(User).filter(User.email == email).first()
     if existing_user:
@@ -14,3 +16,35 @@ def register_user(db: Session, name: str, email: str, password: str):
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Email already registered"
         )
+
+    user = User(
+        name=name,
+        email=email,
+        password_hash=hash_password(password),
+        role="CLIENT"
+    )
+
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return {"message": "User registered successfully"}
+
+
+def authenticate_user(db: Session, email: str, password: str):
+    user = db.query(User).filter(User.email == email).first()
+
+    if not user or not verify_password(password, user.password_hash):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid email or password"
+        )
+
+    token = create_access_token(
+        data={"sub": str(user.id), "role": user.role}
+    )
+
+    return {
+        "access_token": token,
+        "token_type": "bearer"
+    }
